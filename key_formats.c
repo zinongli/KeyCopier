363276ab0f8fdfdcda66407480f109fd843738dc
NamelessNanashi <johnnyjel@pm.me>
2024-10-17 13:19:01 -0500

    update to add more formats

--- key_formats.c
+++ key_formats.c
@@ -19,8 +19,7 @@ const KeyFormat all_formats[] = {
     .min_depth_ind = 1,
     .max_depth_ind = 7,
     .macs = 4,
-    .clearance = 3
-    },
+    .clearance = 3},
 
     {
     .manufacturer = "Schlage",
@@ -38,6 +37,66 @@ const KeyFormat all_formats[] = {
     .depth_step_inch = 0.015,
     .min_depth_ind = 0,
     .max_depth_ind = 9,
+    .macs = 7,
+    .clearance = 8
+    },
+
+    {
+    .manufacturer = "Arrow",
+    .format_name = "AR1",
+    .format_link = "https://lsamichigan.org/Tech/SCHLAGE_KeySpecs.pdf",
+    .first_pin_inch = 0.265,
+    .last_pin_inch = 1.040,
+    .pin_increment_inch = 0.155,
+    .pin_num = 6,
+    .pin_width_inch = 0.060,
+    .elbow_inch = 0.1,
+    .drill_angle = 105, // This should actually be 100 but the current resolution will make 100 degrees very ugly and unsuable
+    .uncut_depth_inch = 0.335,
+    .deepest_depth_inch = 0.186,
+    .depth_step_inch = 0.014,
+    .min_depth_ind = 0,
+    .max_depth_ind = 9,
+    .macs = 7,
+    .clearance = 8
+    },
+
+    {
+    .manufacturer = "Master Lock",
+    .format_name = "M1",
+    .format_link = "https://lsamichigan.org/Tech/SCHLAGE_KeySpecs.pdf",
+    .first_pin_inch = 0.185,
+    .last_pin_inch = 0.689,
+    .pin_increment_inch = 0.126,
+    .pin_num = 5,
+    .pin_width_inch = 0.039,
+    .elbow_inch = 0.1,
+    .drill_angle = 90, // This should actually be 100 but the current resolution will make 100 degrees very ugly and unsuable
+    .uncut_depth_inch = 0.276,
+    .deepest_depth_inch = 0.171,
+    .depth_step_inch = 0.015,
+    .min_depth_ind = 0,
+    .max_depth_ind = 7,
+    .macs = 7,
+    .clearance = 8
+    },
+
+    {
+    .manufacturer = "American",
+    .format_name = "AM7",
+    .format_link = "https://lsamichigan.org/Tech/SCHLAGE_KeySpecs.pdf",
+    .first_pin_inch = 0.157,
+    .last_pin_inch = 0.781,
+    .pin_increment_inch = 0.125,
+    .pin_num = 6,
+    .pin_width_inch = 0.039,
+    .elbow_inch = 0.1,
+    .drill_angle = 90, // This should actually be 100 but the current resolution will make 100 degrees very ugly and unsuable
+    .uncut_depth_inch = 0.238,
+    .deepest_depth_inch = 0.173,
+    .depth_step_inch = 0.016,
+    .min_depth_ind = 0,
+    .max_depth_ind = 8,
     .macs = 7,
     .clearance = 8
     }
