d758e65f621bf65473f4433ac03a24dfa3e3fa1b
HonestLocksmith <haertle@msn.com>
2024-10-19 18:00:04 -0600

    Added Y2, Y11, S22, NA25, CO88
    Minor corrections to previous additions.

--- key_formats.h
+++ key_formats.h
@@ -1,29 +1,29 @@
 #ifndef KEY_FORMATS_H
 #define KEY_FORMATS_H
 
-#define FORMAT_NUM 5
+#define FORMAT_NUM 10
 
 typedef struct {
-  char *manufacturer;
-  char *format_name;
-  char *format_link;
+    char* manufacturer;
+    char* format_name;
+    char* format_link;
 
-  double first_pin_inch;
-  double last_pin_inch;
-  double pin_increment_inch;
-  int pin_num;
-  double pin_width_inch;
-  double drill_angle;
-  double elbow_inch;
+    double first_pin_inch;
+    double last_pin_inch;
+    double pin_increment_inch;
+    int pin_num;
+    double pin_width_inch;
+    double drill_angle;
+    double elbow_inch;
 
-  double uncut_depth_inch;
-  double deepest_depth_inch;
-  double depth_step_inch;
-  int min_depth_ind;
-  int max_depth_ind;
+    double uncut_depth_inch;
+    double deepest_depth_inch;
+    double depth_step_inch;
+    int min_depth_ind;
+    int max_depth_ind;
 
-  int macs;
-  int clearance;
+    int macs;
+    int clearance;
 } KeyFormat;
 
 extern const KeyFormat all_formats[FORMAT_NUM];
